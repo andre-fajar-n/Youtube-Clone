ffbeed4d6f4a3f22865eae7f9eddb094f6594224
andre-fajar-n <afajar@alterra.id>
2020-04-10 16:45:22 +0700

    models

---
+++ youtube/app_youtube/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Users, Videos, Comments, Likes, Dislikes, UserViews
+
+# Register your models here.
+admin.site.register(Users)
+admin.site.register(Videos)
+admin.site.register(Comments)
+admin.site.register(Likes)
+admin.site.register(Dislikes)
+admin.site.register(UserViews)
